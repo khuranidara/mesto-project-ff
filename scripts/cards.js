a7a65fc66695904e7c534e837b88f7ac0e7d6f70
d.khurani <huranidara@gmail.com>
2023-11-01 13:59:00 +0300

    Реализация функционала создания карточек
    Добавлена функция для создания карточек из шаблона, заполнения их данными и отображения на главной странице. Также добавлен обработчик событий для удаления карточек.
    Обновлены начальные ссылки на изображения и описания карточек.

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,27 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Калипсо",
+      link: "https://images.unsplash.com/photo-1531123414780-f74242c2b052?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Затонувшие сокровища",
+      link: "https://images.unsplash.com/photo-1668750651066-cd5988f32233?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Q2FseXBzb3xlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Погружение",
+      link: "https://images.unsplash.com/photo-1691410038685-4fbafa2bbd93?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fCVEMSU4MCVEMSU4QiVEMCVCMSVEMSU4QnxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Морское крыло",
+      link: "https://images.unsplash.com/photo-1579805642405-6e59bb235157?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhzJTJDZiUyMGQlMjAlM0JiZGpxJTIwZ2hiaGpsdHxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Хищник",
+      link: "https://images.unsplash.com/photo-1645522051673-729743445359?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fCVEMCU5QiVEMCVCRSVEMSU4MSVEMCVCRSVEMSU4MSVEMCVCOCUyQyVEMCVCQSVEMCVCMCVEMCVCQiVEMCVCMCVEMCVCRCVEMSU4QnxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Райский риф",
+      link: "https://images.unsplash.com/photo-1548833925-d8ef00b4a8de?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
+
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(data) {
+    // Шаг 1: Клонирование шаблона
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.cloneNode(true);
+    
+    // Шаг 2: Установка значений вложенных элементов
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+  
+    // Шаг 3: Добавление обработчика для иконки удаления
+    const deleteIcon = cardElement.querySelector(".card__delete-button");
+    deleteIcon.addEventListener("click", evt => {
+        const cardElement = evt.target.closest(".card");
+        cardElement.remove();
+    });
+  
+    return cardElement;
+  }
+  
+  const placesList = document.querySelector(".places__list");
+  
+  function renderCards() {
+    initialCards.forEach(cardElement => placesList.append(createCard(cardElement)));
+  }
+  
+  renderCards();
