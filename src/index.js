c1d6495dba05af1888e8180a12a83d97bd55980b
d.khurani <huranidara@gmail.com>
2023-12-10 23:33:14 +0300

    встроенная валидация

--- src/index.js
+++ src/index.js
@@ -48,7 +48,9 @@ addCardForm.addEventListener('submit', handleAddCardFormSubmit);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
+
 function handleFormSubmit(evt) {
+
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
     // О том, как это делать, расскажем позже.
