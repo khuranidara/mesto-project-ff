3903f9d3e3677f93f963cdbc906560b6c2700623
d.khurani <huranidara@gmail.com>
2023-12-14 20:59:12 +0300

    Интеграция с API
    Реализована загрузка информации о пользователе и карточек с сервера.
    Отредактированные данные профиля должны сохраняются на сервере (м. PATCH).
    Добавление новой карточки POST-запросом.

--- src/index.js
+++ src/index.js
@@ -17,11 +17,16 @@ import {
   profilePopup
 } from './scripts/modal.js';
 
-const formElement = document.querySelector('.popup__form[name="edit-profile"]');
-export const nameInput = formElement.querySelector('.popup__input_type_name');
-export const jobInput = formElement.querySelector('.popup__input_type_description');
+const editProfileForm = document.querySelector('.popup__form[name="edit-profile"]');
+export const nameInput = editProfileForm.querySelector('.popup__input_type_name');
+export const jobInput = editProfileForm.querySelector('.popup__input_type_description');
 export const profileName = document.querySelector('.profile__title');
 export const profileJob = document.querySelector('.profile__description');
+const avatarElement = document.querySelector('.profile__image');
+import { renderCards } from './scripts/card.js';
+const cohortId = "wff-cohort-2";
+const token = "f607a7d2-acc8-4a4e-8521-3c1435e4561a";
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -33,11 +38,31 @@ function handleAddCardFormSubmit(evt) {
     const cardName = cardNameInput.value;
     const cardLink = cardLinkInput.value;
 
-    const newCard = createNewCard(cardName, cardLink);
-    placesList.prepend(newCard); // Добавляем карточку в начало списка
+    fetch(`https://nomoreparties.co/v1/${cohortId}/cards`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: cardName,
+            link: cardLink,
+        }),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP ошибка! Статус: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(newCardData => {
+            const newCard = createNewCard(newCardData.name, newCardData.link);
+            placesList.prepend(newCard);// Добавляем карточку в начало списка
 
-    closePopup(addPopup); // Закрываем попап
-    addCardForm.reset(); // Очищаем форму
+            closePopup(addPopup);
+            addCardForm.reset(); // Очищаем форму
+        })
+        .catch(error => console.error('Ошибка:', error));
 }
 
 // Прикрепляем обработчик к форме:
@@ -47,23 +72,90 @@ addCardForm.addEventListener('submit', handleAddCardFormSubmit);
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 
-function handleFormSubmit(evt) {
+function handleEditProfileFormSubmit(evt) {
 
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
     // О том, как это делать, расскажем позже.
 
     const nameValue = nameInput.value;
-    const jobValue = jobInput.value; // Получите значение полей jobInput и nameInput из свойства value
+    const jobValue = jobInput.value; // Получаем значение полей jobInput и nameInput из свойства value
 
     profileName.textContent = nameValue;
-    profileJob.textContent = jobValue; // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
+    profileJob.textContent = jobValue; // Выбераем элементы, куда должны быть вставлены значения полей
+    // Вставляем новые значения с помощью textContent
+    // PATCH-запрос на сервер
+    fetch(`https://nomoreparties.co/v1/${cohortId}/users/me`, {
+        method: 'PATCH',
+        headers: {
+            'Authorization': `${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: nameValue,
+            about: jobValue,
+        }),
+    })
+        .then(response => {
+            console.log(response); // Вывести ответ сервера в консоль
+            if (!response.ok) {
+                throw new Error(`HTTP ошибка! Статус: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(updatedUserData => {
+            avatarElement.style.backgroundImage = `url(${updatedUserData.avatar})`;
+            // Обновление данные на странице с использованием полученных данных
+            profileName.textContent = updatedUserData.name;
+            profileJob.textContent = updatedUserData.about;
 
-    // Закрытие попапа после успешного сабмита
-    closePopup(profilePopup);
+            // Закрываем попап редактирования профиля
+            closePopup(profilePopup);
+        })
+        .catch(error => console.error('Ошибка:', error));
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+editProfileForm.addEventListener('submit', handleEditProfileFormSubmit);
+
+// Запрос на данные пользователя
+const userDataRequest = fetch(`https://nomoreparties.co/v1/${cohortId}/users/me`, {
+    method: 'GET',
+    headers: {
+        'Authorization': `${token}`,
+        'Content-Type': 'application/json',
+    },
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP ошибка! Статус: ${response.status}`);
+        }
+        return response.json();
+    });
+
+// Запрос на карточки
+const cardsRequest = fetch(`https://nomoreparties.co/v1/${cohortId}/cards`, {
+    method: 'GET',
+    headers: {
+        'Authorization': `${token}`,
+        'Content-Type': 'application/json',
+    },
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP ошибка! Статус: ${response.status}`);
+        }
+        return response.json();
+    });
+Promise.all([userDataRequest, cardsRequest])
+    .then(([userData, cards]) => {
+        // Обновление элементов в шапке страницы с использованием данных пользователя
+        profileName.textContent = userData.name; // Используем существующую переменную
+        profileJob.textContent = userData.about; // Используем существующую переменную
+        avatarElement.style.backgroundImage = `url(${userData.avatar})`;
+
+        // Обновление карточек на странице с использованием данных с сервера
+        renderCards(cards);
+    })
+    .catch(error => console.error('Ошибка:', error));
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,16 +1,13 @@
-import { initialCards } from './cards.js';
 import { openImagePopup } from './modal.js';
 
-// Константы для работы с DOM-элементами
 export const addCardForm = document.querySelector('.popup__form[name="new-place"]');
 export const cardNameInput = addCardForm.querySelector('.popup__input_type_card-name');
 export const cardLinkInput = addCardForm.querySelector('.popup__input_type_url');
-export const placesList = document.querySelector(".places__list"); // Список для отрисовки карточек
-export const cardTemplate = document.querySelector("#card-template").content; // Шаблон карточки
+export const placesList = document.querySelector(".places__list");
+export const cardTemplate = document.querySelector("#card-template").content;
 
-// Функция для лайка карточки
 function handleLikeClick(evt) {
-  evt.target.classList.toggle('card__like-button_is-active');
+    evt.target.classList.toggle('card__like-button_is-active');
 }
 
 function handleDeleteCard(evt) {
@@ -20,36 +17,32 @@ function handleDeleteCard(evt) {
 function handleCardClick(imageSrc, imageName) {
     openImagePopup(imageSrc, imageName);
 }
-// Функция для создания карточки
+
 export function createCard(data, handleLike, handleDeleteCard, handleCardClick) {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardImage = cardElement.querySelector(".card__image");
-  const cardTitle = cardElement.querySelector(".card__title");
-  const likeButton = cardElement.querySelector('.card__like-button');
-  const deleteIcon = cardElement.querySelector(".card__delete-button");
-  
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
-  cardTitle.textContent = data.name;
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    const likeButton = cardElement.querySelector('.card__like-button');
+    const deleteIcon = cardElement.querySelector(".card__delete-button");
 
-  deleteIcon.addEventListener("click", handleDeleteCard);
-  cardImage.addEventListener('click', () => handleCardClick(data.link, data.name));
-  likeButton.addEventListener('click', handleLike);
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
 
-  return cardElement;
+    deleteIcon.addEventListener("click", handleDeleteCard);
+    cardImage.addEventListener('click', () => handleCardClick(data.link, data.name));
+    likeButton.addEventListener('click', handleLike);
+
+    return cardElement;
 }
 
-// Функция для отрисовки начальных карточек
-export function renderCards(initialCards) {
-  initialCards.forEach(cardData => {
+export function renderCards(cards) {
+    cards.forEach(cardData => {
         const cardElement = createCard(cardData, handleLikeClick, handleDeleteCard, handleCardClick);
-      placesList.append(cardElement);
-  });
+        placesList.append(cardElement);
+    });
 }
 
-renderCards(initialCards);
-
-// Функция для создания новой карточки
 export function createNewCard(name, link) {
-    return  createCard({ name, link }, handleLikeClick, handleDeleteCard, handleCardClick);
+    return createCard({ name, link }, handleLikeClick, handleDeleteCard, handleCardClick);
 }
