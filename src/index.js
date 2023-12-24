ee6a55902d87943df299567c0856bb5fc5fe2258
d.khurani <huranidara@gmail.com>
2023-12-24 12:33:17 +0300

    Исправление замечаний ревьюера 4
    Исправлен модуль modal.js, теперь он не содержит глобальных переменных. Блок работы с аватаром восстановлен.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './pages/index.css'; // добавьте импорт главного файла стилей
 import './scripts/validation.js';
 import './scripts/card.js';
-import './scripts/modal.js';
 import {
     addCardForm,
     cardNameInput,
@@ -11,14 +10,9 @@ import {
     renderCards
 } from './scripts/card.js';
 import {
-    imagePopup,
-    popupImage,
-    popupCaption,
-    closePopup,
-    addPopup,
-    profilePopup, editButton, openPopup, addButton
-} from './scripts/modal.js';
-
+    openPopup,
+    closePopup
+} from "./scripts/modal";
 import {enableValidation,clearValidation} from './scripts/validation';
 import * as api from './scripts/api.js';
 
@@ -33,8 +27,19 @@ export let currentUserId;
 const newPlacePopup = document.querySelector('.popup_type_new-card');
 const newPlacePopupForm = newPlacePopup.querySelector('.popup__form');
 const newPlaceSubmitButton = newPlacePopupForm.querySelector('.button');
+const profilePopup = document.querySelector('.popup_type_edit');
 const editProfilePopupForm=  profilePopup.querySelector('.popup__form');
 const editProfileSubmitButton = editProfilePopupForm.querySelector('.button');
+const updateAvatarPopupInput = updateAvatarPopup.querySelector('.popup__input_type_avatar-link');
+const updateAvatarPopupForm =updateAvatarPopup.querySelector('.popup__form');
+const updateAvatarSubmitButton = updateAvatarPopupForm.querySelector('.button');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const addPopup = document.querySelector('.popup_type_new-card');
+const popups = document.querySelectorAll('.popup');
+const imagePopup = document.querySelector('.popup_type_image');
+const popupImage = imagePopup.querySelector('.popup__image');
+const popupCaption = imagePopup.querySelector('.popup__caption');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -91,6 +96,24 @@ function handleFormSubmit(evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 
+updateAvatarPopup.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newAvatarLink = updateAvatarPopupInput.value;
+    updateAvatarSubmitButton.textContent = 'Сохранение...';
+    api.updateAvatar(newAvatarLink)
+        .then(updatedUserData => {
+            // Обновление изображения аватара на странице
+            avatarElement.style.backgroundImage = `url(${updatedUserData.avatar})`;
+            // Закрытие формы после успешного обновления
+            closePopup(updateAvatarPopup);
+        })
+        .catch(error => console.error('Ошибка:', error))
+        .finally(() => {
+            // Возвращение исходного текста кнопки
+            updateAvatarSubmitButton.textContent = 'Сохранить';
+        });
+});
+
 const validationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
@@ -101,6 +124,11 @@ const validationConfig = {
 };
 
 enableValidation(validationConfig);
+popups.forEach( function(b) {
+    b.addEventListener('mouseup', (evt) => {
+        evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup_is-opened') ? closePopup(b) : null;
+    })
+});
 
 editButton.addEventListener('click', function() {
     nameInput.value = profileName.textContent;
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,21 +1,6 @@
-export const editButton = document.querySelector('.profile__edit-button');
-export const addButton = document.querySelector('.profile__add-button');
-export const addPopup = document.querySelector('.popup_type_new-card');
-export const profilePopup = document.querySelector('.popup_type_edit');
-export const popups = document.querySelectorAll('.popup');
-export const imagePopup = document.querySelector('.popup_type_image');
-export const popupImage = imagePopup.querySelector('.popup__image');
-export const popupCaption = imagePopup.querySelector('.popup__caption');
-
 export function esc(evt) {
     evt.code === 'Escape' && closePopup(document.querySelector('.popup_is-opened'));
 }
-
-popups.forEach( function(b) {
-    b.addEventListener('mouseup', (evt) => {
-        evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup_is-opened') ? closePopup(b) : null;
-    })
-});
 
 export function openPopup(popupElement) {
     popupElement.classList.add('popup_is-opened');
